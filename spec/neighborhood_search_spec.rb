d4660196491cfed51fe9d0a0dc456873cacd4957
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2013-06-29 14:13:18 +1000

    Update specs to use Yelp module

--- spec/neighborhood_search_spec.rb
+++ spec/neighborhood_search_spec.rb
@@ -1,29 +1,31 @@
 require 'spec_helper'
 
-describe 'Neighborhood Search' do
-  let!(:client) { create_client(AdditionalSpecHelpers::API_V1) }
+module Yelp::V1::Neighborhood::Request
+  describe 'Neighborhood Search' do
+    let!(:client) { create_client(AdditionalSpecHelpers::API_V1) }
 
-  describe 'by GeoPoint' do
-    it 'returns neighbourhoods at point' do
-      request = Yelp::V1::Neighborhood::Request::GeoPoint.new(:latitude => 37.782093,
-                                                              :longitude => -122.483230,
-                                                              :yws_id => @yws_id)
-      response = client.search(request)
-      expect(response).to be_valid_response_hash
-      expect(response['neighborhoods'].first['name']).to eq('Outer Richmond')
+    describe 'by GeoPoint' do
+      it 'returns neighbourhoods at point' do
+        request = GeoPoint.new(:latitude => 37.782093,
+                               :longitude => -122.483230,
+                               :yws_id => @yws_id)
+        response = client.search(request)
+        expect(response).to be_valid_response_hash
+        expect(response['neighborhoods'].first['name']).to eq('Outer Richmond')
+      end
     end
-  end
 
-  describe 'by Location' do
-    it 'returns neighbourhoods at location' do
-      request = Yelp::V1::Neighborhood::Request::Location.new(:address => '2252 Clement Street',
-                                                              :city => 'San Francisco',
-                                                              :state => 'CA',
-                                                              :zipcode => 94121,
-                                                              :yws_id => @yws_id)
-      response = client.search(request)
-      expect(response).to be_valid_response_hash
-      expect(response['neighborhoods'].first['name']).to eq('Outer Richmond')
+    describe 'by Location' do
+      it 'returns neighbourhoods at location' do
+        request = Location.new(:address => '2252 Clement Street',
+                               :city => 'San Francisco',
+                               :state => 'CA',
+                               :zipcode => 94121,
+                               :yws_id => @yws_id)
+        response = client.search(request)
+        expect(response).to be_valid_response_hash
+        expect(response['neighborhoods'].first['name']).to eq('Outer Richmond')
+      end
     end
   end
 end
